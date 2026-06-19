422ab99bd7016db64a4fc9f7fa2c0fe925110664
maressabelas <152742800+maressabelas@users.noreply.github.com>
2026-06-19 00:31:59 -0300

    Update ft_puthexa.c

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -16,17 +16,19 @@ int	ft_puthexa(unsigned int n, char c)
 {
 	int				count;
 	char			*base;
+	int				aux;
 
 	count = 0;
 	if (c == 'x')
 		base = "0123456789abcdef";
 	else
 		base = "0123456789ABCDEF";
-	if (n == 0)
-		return (write(1, "0", 1));
 	if (n >= 16)
 	{
-		count += ft_puthexa(n / 16, c);
+		aux = ft_puthexa(n / 16, c);
+		if (aux == -1)
+        	return (-1);
+		count += aux;
 	}
 	n %= 16;
 	if (write(1, &base[n], 1) == -1)
@@ -34,6 +36,29 @@ int	ft_puthexa(unsigned int n, char c)
 	count++;
 	return (count);
 }
+
+static int	ft_puthexa_long(unsigned long n)
+{
+	int	count;
+	int	aux;
+	char *base;
+
+	count = 0;
+	base = "0123456789abcdef";
+	if (n >= 16)
+	{
+		aux = ft_puthexa_long(n / 16);
+		if (aux == -1)
+			return (-1);
+		count += aux;
+	}
+	n %= 16;
+	if (write(1, &base[n], 1) == -1)
+		return (-1);
+	count++;
+	return (count);
+}
+
 
 int	ft_putptr(void *ptr)
 {
@@ -43,16 +68,16 @@ int	ft_putptr(void *ptr)
 	len = 0;
 	if (!ptr)
 	{
-		write(1, "(nil)", 5);
+		if (write(1, "(nil)", 5) == -1);
+			return (-1);
 		return (5);
 	}
-	ft_putstr("0x");
-	len += 2;
-	hexlen = ft_puthexa((unsigned long)ptr, 'x');
-	if (hexlen < 0)
-	{
+	if (ft_putstr("0x") == -1)
 		return (-1);
-	}
+	len += 2;
+	hexlen = ft_puthexa_long((unsigned long)ptr);
+	if (hexlen < 0)
+		return (-1);
 	len += hexlen;
 	return (len);
 }
